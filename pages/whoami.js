195b697081036f2019f38cb4b432be74f78ea399
Leon Pupier <leonpupier@gmail.com>
2024-03-28 18:01:27 +0100

    Complete the WhoAmI page

--- pages/whoami.js
+++ pages/whoami.js
@@ -9,7 +9,7 @@ function renderWhoAmIPage() {
 					<img id="user-img" src="img/leon.jpg" alt="User picture" />
 
 					<h2><span>Who am I?</span></h2>
-					<p>
+					<p id="paragraph">
 					My name is Léon Pupier and I am a french student at 
 					<span id="external-link-text" onclick="window.open('https://www.42.fr/', '_blank');">school 42</span>.
 					My passion for computer science and more particularly the creation of programs and software solutions
@@ -24,19 +24,54 @@ function renderWhoAmIPage() {
 
 			<div class="section">
 				<div class="section-content">
-					<h2>Skills</h2>
-					<p>Some skills...</p>
-				</div>
-			</div>
 
-			<div class="section">
-				<div class="section-content">
-					<h2>Education</h2>
-					<p>Some education...</p>
+					<img id="projects-img" src="img/projects.png" alt="Projects picture" />
+
+					<h2><span>Why a website?</span></h2>
+					<p id="paragraph">
+					You can find my main projects on this website, as well as a non-exhaustive list of my skills.
+					Not forgetting an overview of my career.
+					This site also serves as a showcase and contact point.
+					Thank you for taking the time to look at my creations, I hope to continue producing quality work.
+					</p>
+					</br>
+					<p id="paragraph-sign"><span>Léon Pupier</span></p>
+				
 				</div>
 			</div>
 		</div>
-    `;
+	`;
+
+	const sections = document.querySelectorAll('.section');
+    sections.forEach((section, index) => {
+        // Add 'even' or 'odd' class based on the index
+        section.classList.add(index % 2 === 0 ? 'even' : 'odd');
+    });
+
+    // Handle the animation of the sections
+    window.addEventListener('scroll', function() {
+        sections.forEach((section, index) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const sectionBottom = section.getBoundingClientRect().bottom;
+            if (sectionTop <= window.innerHeight && sectionBottom >= 0) {
+                section.classList.add('animate');
+                if (index !== 0 || sectionBottom >= window.innerHeight) {
+                    setTimeout(function() {
+                        section.querySelector('.section-content').classList.add('show-text');
+                    }, 1000);
+                }
+            }
+        });
+    });
+
+	// Trigger the text animation for the first section immediately
+	setTimeout(function() {
+		sections[0].classList.add('animate');
+	}, 0);
+
+	setTimeout(function() {
+		document.querySelector('.section:first-child .section-content').classList.add('show-text');
+	}, 1000);
 
 	// Handle the click on a span link
 	document.getElementById('external-link').addEventListener('click', function() {
@@ -45,7 +80,6 @@ function renderWhoAmIPage() {
 
 	// Handle the navigation when the user scrolls
 	let currentSectionIndex = 0;
-	const sections = document.querySelectorAll('.section');
 	let isScrolling = false;
 
 	function scrollSection(index) {
